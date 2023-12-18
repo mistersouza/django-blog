ce2c818a9d274c0a0aa6ff60dd7882be1fb0da35
Thiago Souza <mistersouza@icloud.com>
2023-12-18 13:45:48 +0000

    Rename blog view to blog

--- codestar/codestar/urls.py
+++ codestar/codestar/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from blog import views as blog_views
 
 urlpatterns = [
-    path('blog/', blog_views.index, name='blog'),
+    path('blog/', blog_views.blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
