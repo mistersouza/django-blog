bc11ea142529d1883f08348769cfed423983f762
Thiago Souza <mistersouza@icloud.com>
2023-12-19 14:51:43 +0000

    Add str dunder and meta ordering to models

--- blog/models.py
+++ blog/models.py
@@ -15,8 +15,13 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
 
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    
     def __str__(self):
-        return self.title
+        return f'{self.title} | written by {self.author.first_name}'
 
 
 class Comment(models.Model):
@@ -27,3 +32,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ["created_on"]
+
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
